412b9c250058a7adfedf3b784cbe4fcfc744cd23
nguyennhung1529 <nguyenthinhung1529@gmail.com>
2022-04-02 20:10:29 +0700

    Revert "fix some warning"
    This reverts commit 94d386767119c497560bf8a6c1e0192be3803133.

--- app/src/main/java/com/example/simplebackgroundtask/UserDetailActivity.java
+++ app/src/main/java/com/example/simplebackgroundtask/UserDetailActivity.java
@@ -9,8 +9,11 @@ import android.widget.TextView;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 public class UserDetailActivity extends AppCompatActivity {
     private TextView mUser;
+    private JSONObject jObject = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
